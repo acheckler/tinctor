feff0e0b205014cae6c8395e3aa72a29cfc314c9
acheckler <allisonheckler@gmail.com>
2021-06-30 18:48:21 -0500

    using server, all endpoints funcional

--- src/App/App.js
+++ src/App/App.js
@@ -1,66 +1,131 @@
 import React, { Component } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import dummyStore from "../dummy-store";
 import AddProject from "../AddProject/AddProject";
 import ApiContext from "../ApiContext";
 import LandingPage from "../Home/LandingPage";
 import ProjectPage from "../ProjectPage/ProjectPage";
-import "./App.css"
-
+import "./App.css";
+import config from "../config";
+import PropTypes from 'prop-types'
 
 class App extends Component {
   state = {
-    cannabinoids: [],
-    carriers: [],
-    flavors: [],
-    bottles: [],
-    droppers: [],
+    projects: [],
+    projectsDisplay: [],
+    ingredients: [],
+    packaging: [],
+  };
+  static propTypes = {
+    history: PropTypes.object,
+  };
+
+  fetchData = () => {
+    Promise.all([
+      fetch(`${config.API_ENDPOINT}/projects`),
+      fetch(`${config.API_ENDPOINT}/projects/display`),
+      fetch(`${config.API_ENDPOINT}/ingredients`),
+      fetch(`${config.API_ENDPOINT}/packaging`),
+    ])
+      .then(
+        ([projectsRes, projectsDisplayRes, ingredientsRes, packagingRes]) => {
+          if (!projectsRes.ok)
+            return projectsRes.json().then((e) => Promise.reject(e));
+          if (!projectsDisplayRes.ok)
+            return projectsDisplayRes.json().then((e) => Promise.reject(e));
+          if (!ingredientsRes.ok)
+            return ingredientsRes.json().then((e) => Promise.reject(e));
+          if (!packagingRes.ok)
+            return packagingRes.json().then((e) => Promise.reject(e));
+
+          return Promise.all([
+            projectsRes.json(),
+            projectsDisplayRes.json(),
+            ingredientsRes.json(),
+            packagingRes.json(),
+          ]);
+        }
+      )
+      .then(([projects, projectsDisplay, ingredients, packaging]) => {
+        this.setState({ projects, projectsDisplay, ingredients, packaging });
+      })
+      .catch((error) => {
+        console.error({ error });
+      });
   };
   componentDidMount() {
-    // fake date loading from API call
-    setTimeout(() => this.setState(dummyStore), 600);
+    this.fetchData();
   }
 
- calculateCost(tincVolume, cannaConcentration) {
-    const cannaCost = (this.state.cannaId * cannaConcentration) / 1000;
-    const carrierCost = tincVolume * this.state.carrierId;
-    const packagingCost = this.state.bottleId + this.state.dropperId;
-    const totalCost = cannaCost + carrierCost + packagingCost;
-    console.log(totalCost, "hiiiii");
+  handleAddProject = (formData, totalCpu) => {
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: formData.name,
+        t_volume: formData.tVol,
+        canna_id: formData.canna,
+        canna_concentration: formData.cCon,
+        carrier_id: formData.carr,
+        carrier_percentage: formData.carrP,
+        flavor_id: formData.flav,
+        bottle_id: formData.bottle,
+        dropper_id: formData.dropper,
+        total_cpu: formData.cpu
+      }),
+    };
+
+  
+    fetch(`${config.API_ENDPOINT}/projects`, options)
+         .then((projectsRes) => {
+        if (!projectsRes.ok) {
+          throw new Error("error");
+        }
+        return projectsRes.json();
+      })
+      .then(() => {this.fetchData()})
+      .catch((error) => console.log(error));
+  };
+
+  handleDeleteProject = (id) => {
+    this.setState({
+      projects: this.state.projects.filter((project) => project.id !== id),
+    })
+
+   console.log('setState ran')
+    this.fetchData()
   }
 
   render() {
     const value = {
-      cannabinoids: this.state.cannabinoids,
-      carriers: this.state.carriers,
-      flavors: this.state.flavors,
-      bottles: this.state.bottles,
-      droppers: this.state.droppers,
       projects: this.state.projects,
-      calculateCost: this.calculateCost
+      projectsDisplay: this.state.projectsDisplay,
+      ingredients: this.state.ingredients,
+      packaging: this.state.packaging,
+      addProject: this.handleAddProject,
+      deleteProject: this.handleDeleteProject
     };
 
     return (
       <ApiContext.Provider value={value}>
-        <div className="App" >
+        <div className="App">
           <header className="App_header">
-            
             <h1 className="App_header_box">
-              <Link to="/" className="header_link">Tinctor</Link> <FontAwesomeIcon icon="calculator" />
+              <Link to="/" className="header_link">
+                Tinctor
+              </Link>{" "}
+              <FontAwesomeIcon icon="calculator" />
             </h1>
           </header>
           <div className="nav-buttons">
             <Switch>
-            <Route exact path ="/" component={LandingPage} />
-            <Route path="/newProject" component={AddProject} />
-            <Route path="/projects/:projectName" component={ProjectPage} />
+              <Route exact path="/" component={LandingPage} />
+              <Route path="/newProject" component={AddProject} />
+              <Route path="/projects/:projectName" component={ProjectPage} />
             </Switch>
-            {/* {this.renderMainRoutes()} */}
-            {/* <Link to="/newProject">
-              <button className="new-p-button">New Project</button>
-            </Link> */}
-            {/* <AddProject /> */}
           </div>
         </div>
       </ApiContext.Provider>
--- src/Home/LandingPage.js
+++ src/Home/LandingPage.js
@@ -1,41 +1,54 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import ApiContext from "../ApiContext";
-import "./LandingPage.css"
+import "./LandingPage.css";
 
 class LandingPage extends Component {
-    static contextType = ApiContext;
-    render() {
-        const {projects=[]} = this.context
-    return(
-        <div className="lp-container">
-            <div className="app-intro">
-            <h2>Welcome to Tinctor!</h2>
-            <p className="p-intro">Originally built for 'BATCH by Wisconsin Hemp Scientific', Tinctor was made to streamline the quotation process with clients. Many factors go into the pricing of tincture formulas, and Tinctor allows the business to give their clients immediate answers to their queries, instead of searching for pieces of data throughout countless spreadsheets and doing the calculations themselves. </p>
-            </div>
-            <div className="lp-container2">
-            <h2>Projects:</h2>
-            <ol className="project_list">
-            
-                {projects.map(project =>
-                    <li key={project.name}>
-                    {/* <ProjectPage
-                    id={project.id}
-                    name={project.name}
-                    totalCPU={project.totalCPU}
-                    /> */}
-                        <Link to={`/projects/${project.name}`} className="project-link">{project.name}</Link>
-                    </li>
-                    )}
+  static contextType = ApiContext;
 
-            </ol>
-        <Link to="/newProject">
-        <button className="new-p-button">New Project</button>
-      </Link>
+  getProject(projectId) {
+    this.context.projectsDisplay.filter((project) => project.id === projectId);
+  }
+
+  render() {
+    const { projects = [] } = this.context;
+    return (
+      <div className="lp-container">
+        <div className="app-intro">
+          <h2>Welcome to Tinctor!</h2>
+          <p className="p-intro">
+            Originally built for 'BATCH by Wisconsin Hemp Scientific', Tinctor
+            was made to streamline the quotation process with clients. Many
+            factors go into the pricing of tincture formulas, and Tinctor allows
+            the business to give their clients immediate answers to their
+            queries, instead of searching for pieces of data throughout
+            countless spreadsheets and doing the calculations themselves.{" "}
+          </p>
+        </div>
+        <div className="lp-container2">
+          <h2>Projects:</h2>
+          <ol className="project_list">
+            {projects.map((project) => (
+              <li key={project.id}>
+                <Link
+                  to={{
+                    pathname: `/projects/${project.name}`,
+                    state: { project: [project], projectId: project.id },
+                  }}
+                >
+                  {project.name}
+                </Link>
+              </li>
+            ))}
+          </ol>
+          <Link to="/newProject">
+            <button className="new-p-button">New Project</button>
+          </Link>
+        </div>
       </div>
-      </div>
-    )
-    }
+    );
+  }
 }
 
-export default LandingPage
+export default LandingPage;
+
